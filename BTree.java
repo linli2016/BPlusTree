417a9b3deb119af0c28bba32a305b1c49c43ca00
Constantinos Pisimisis <93882053+CodeMaestro1@users.noreply.github.com>
2023-12-25 16:13:05 +0200

    Update BTree.java
    Custom order of the tree is now available

--- BTree.java
+++ BTree.java
@@ -1,64 +1,101 @@
+package org.tuc.btree;
+
+import org.tuc.utils.MultiCounter;
+
 /**
- * A B+ tree
- * Since the structures and behaviors between internal node and external node are different, 
- * so there are two different classes for each kind of node.
- * @param <TKey> the data type of the key
+ * Represents a B+ tree data structure.
+ * The structures and behaviors between internal nodes and external nodes are different,
+ * so there are separate classes for each kind of node.
+ *
+ * @param <TKey>   the data type of the key
  * @param <TValue> the data type of the value
  */
 public class BTree<TKey extends Comparable<TKey>, TValue> {
-	private BTreeNode<TKey> root;
-	
-	public BTree() {
-		this.root = new BTreeLeafNode<TKey, TValue>();
-	}
 
-	/**
-	 * Insert a new key and its associated value into the B+ tree.
-	 */
-	public void insert(TKey key, TValue value) {
-		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
-		leaf.insertKey(key, value);
-		
-		if (leaf.isOverflow()) {
-			BTreeNode<TKey> n = leaf.dealOverflow();
-			if (n != null)
-				this.root = n; 
-		}
-	}
-	
-	/**
-	 * Search a key value on the tree and return its associated value.
-	 */
-	public TValue search(TKey key) {
-		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
-		
-		int index = leaf.search(key);
-		return (index == -1) ? null : leaf.getValue(index);
-	}
-	
-	/**
-	 * Delete a key and its associated value from the tree.
-	 */
-	public void delete(TKey key) {
-		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
-		
-		if (leaf.delete(key) && leaf.isUnderflow()) {
-			BTreeNode<TKey> n = leaf.dealUnderflow();
-			if (n != null)
-				this.root = n; 
-		}
-	}
-	
-	/**
-	 * Search the leaf node which should contain the specified key
-	 */
-	@SuppressWarnings("unchecked")
-	private BTreeLeafNode<TKey, TValue> findLeafNodeShouldContainKey(TKey key) {
-		BTreeNode<TKey> node = this.root;
-		while (node.getNodeType() == TreeNodeType.InnerNode) {
-			node = ((BTreeInnerNode<TKey>)node).getChild( node.search(key) );
-		}
-		
-		return (BTreeLeafNode<TKey, TValue>)node;
-	}
+    /** The root node of the B+ tree. */
+    private BTreeNode<TKey> root;
+
+    /** The order (maximum number of keys) for the leaf nodes. */
+    protected static int leafOrder;
+
+    /**
+     * Instantiates a new BTree with a custom leaf order.
+     *
+     * @param leafOrder the leaf order
+     */
+    public BTree(int leafOrder) {
+        BTree.leafOrder = leafOrder;
+        this.root = new BTreeLeafNode<>(leafOrder);
+    }
+
+    /**
+     * Inserts a new key and its associated value into the B+ tree.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    public void insert(TKey key, TValue value) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+        leaf.insertKey(key, value);
+
+        if (leaf.isOverflow()) {
+            BTreeNode<TKey> n = leaf.dealOverflow();
+            if (n != null)
+                this.root = n;
+        }
+    }
+
+    /**
+     * Searches for a key in the B+ tree and returns its associated value.
+     *
+     * @param key the key
+     * @return the value associated with the key, or null if the key is not found
+     */
+    public TValue search(TKey key) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+
+        int index = leaf.search(key);
+        return (index == -1) ? null : leaf.getValue(index);
+    }
+
+    /**
+     * Deletes a key and its associated value from the B+ tree.
+     *
+     * @param key the key to be deleted
+     */
+    public void delete(TKey key) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+
+        if (leaf.delete(key) && leaf.isUnderflow()) {
+            BTreeNode<TKey> n = leaf.dealUnderflow();
+            if (n != null)
+                this.root = n;
+        }
+    }
+
+    /**
+     * Searches for the leaf node that should contain the specified key.
+     *
+     * @param key the key
+     * @return the leaf node that should contain the key
+     */
+    @SuppressWarnings("unchecked")
+    private BTreeLeafNode<TKey, TValue> findLeafNodeShouldContainKey(TKey key) {
+        BTreeNode<TKey> node = this.root;
+        // in case root is the only node, the counter will still get increased.
+        while (MultiCounter.increaseCounter(1) && node.getNodeType() == TreeNodeType.InnerNode) {
+            node = ((BTreeInnerNode<TKey>) node).getChild(node.search(key));
+        }
+
+        return (BTreeLeafNode<TKey, TValue>) node;
+    }
+
+    /**
+     * Retrieves the leaf order of the B+ tree.
+     *
+     * @return the leaf order
+     */
+    public static int getLeafOrder() {
+        return leafOrder;
+    }
 }
